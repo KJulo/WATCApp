113c969854a71e69397f5a8aeeb9776d403e0368
Geraldmo122 <81269879+Geraldmo122@users.noreply.github.com>
2022-06-04 22:03:42 -0400

    fix

--- app/src/main/java/com/example/watc/enviarAlerta.kt
+++ app/src/main/java/com/example/watc/enviarAlerta.kt
@@ -4,16 +4,41 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class enviarAlerta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enviar_alerta)
 
-        val btn: Button = findViewById(R.id.enviar)
-        btn.setOnClickListener {
-            val intent: Intent = Intent(this, Home::class.java)
-            startActivity(intent)
-        }
+          val btn: Button = findViewById(R.id.enviar)
+          btn.setOnClickListener {
+              val intent: Intent = Intent(this, Home::class.java)
+              startActivity(intent)
+
+
+              val toast = Toast.makeText(this, "La alerta a sido resivida, enviaremos un guardia al sector del problema", Toast.LENGTH_LONG)
+              toast.show()
+
+          }
+
+       /* val enviar: Button = findViewById(R.id.enviar)
+        enviar.setOnClickListener{
+            val builder = AlertDialog.Builder(this@enviarAlerta)
+            val view = layoutInflater.inflate(R.layout.alerta_enviada, null)
+            builder.setView(view)
+
+            val enviada= builder.create()
+            enviada.show()
+
+
+            view.setOnClickListener {
+                val intent: Intent = Intent(this, Home::class.java)
+                startActivity(intent)
+            }
+        }*/
+
+
     }
 }
