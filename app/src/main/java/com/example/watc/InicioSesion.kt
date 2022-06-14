d0374acc432a37507b672aaa6c00dc1357fe7aa5
Ignacio Silva <kjulo.c@gmail.com>
2022-06-13 21:47:42 -0400

    :sparkles: added login admin & login user

--- app/src/main/java/com/example/watc/InicioSesion.kt
+++ app/src/main/java/com/example/watc/InicioSesion.kt
@@ -3,7 +3,16 @@ package com.example.watc
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
+import com.example.watc.service.ApiInterface
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
 
 class InicioSesion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,4 +26,6 @@ class InicioSesion : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+
 }
--- app/src/main/java/com/example/watc/InicioSesionAdmin.kt
+++ app/src/main/java/com/example/watc/InicioSesionAdmin.kt
@@ -2,27 +2,72 @@ package com.example.watc
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.example.watc.entity.checkUser
+import com.example.watc.service.ApiInterface
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class InicioSesionAdmin : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_admin)
 
         val btn: Button = findViewById(R.id.volver)
         val btnLogin: Button = findViewById(R.id.login)
 
-        btnLogin.setOnClickListener{
-            val intent: Intent = Intent(this, HomeAdmin:: class.java)
+        btnLogin.setOnClickListener {
+            val email: EditText = findViewById(R.id.editTextTextPersonName2)
+            val contrasena: EditText = findViewById(R.id.editTextTextPassword2)
+            var textEmail = email.text
+            var textContrasena = contrasena.text
+            checkAdmin(textEmail,textContrasena)
+        }
+
+        btn.setOnClickListener {
+            val intent: Intent = Intent(this, InicioSesionLogin::class.java)
             startActivity(intent);
         }
 
-        btn.setOnClickListener{
-            val intent: Intent = Intent(this, InicioSesionLogin:: class.java)
-            startActivity(intent);
-        }
 
+    }
+    /*btnLogin.setOnClickListener{
+        val intent: Intent = Intent(this, HomeAdmin:: class.java)
+        startActivity(intent);
+    }*/
 
+    private fun changeMainAdmin(){
+        val intent: Intent = Intent(this, HomeAdmin:: class.java)
+        startActivity(intent);
+    }
+
+    private fun checkAdmin(textEmail: Editable, textContrasena: Editable){
+        val retrofitBuilder = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(ApiInterface::class.java)
+        val retrofitData = retrofitBuilder.checkAdmin(textEmail.toString(),textContrasena.toString())
+
+        retrofitData.enqueue(object : Callback<checkUser?> {
+            override fun onResponse(call: Call<checkUser?>, response: Response<checkUser?>) {
+                val responseBody = response.body()!!
+                val myStringBuilder = StringBuilder()
+                myStringBuilder.append(responseBody.login.exists)
+                if (responseBody.login.exists == true){
+                    changeMainAdmin()
+                }
+            }
+
+            override fun onFailure(call: Call<checkUser?>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
