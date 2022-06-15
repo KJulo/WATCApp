d737f372a2db44e10eb2ea08ae25f6eff17437cb
PatricioCataldo <pato.elias15@gmail.com>
2022-06-15 04:16:32 -0400

    added camera 2.1

--- app/src/main/java/com/example/watc/enviarAlerta.kt
+++ app/src/main/java/com/example/watc/enviarAlerta.kt
@@ -6,6 +6,12 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.os.Build
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.content.pm.PackageManager
 import android.os.Environment
 import android.provider.MediaStore
 import android.widget.Button
@@ -14,54 +20,78 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_enviar_alerta.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
+
 import java.io.File
 
-private val SELECT_ACTIVITY =1
-private val SELECT_ACTIVITY2 =2
-private val FILE_NAME = "photo.jpg"
-private lateinit var photoFile : File
+private const val SELECT_ACTIVITY =1
+private const val PERMISSION_CODE= 2
+private const val IMAGE_CAPTURE_CODE= 3
+
 
 class enviarAlerta : AppCompatActivity() {
 
     private var imageUri: Uri? = null
+    private var imageUri2: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enviar_alerta)
         cargarFoto.setOnClickListener {
-            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            photoFile= getPhotoFile(FILE_NAME)
-            val fileProvider = FileProvider.getUriForFile(this,"com.example.watc.fileprovider",
-                photoFile)
-            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,fileProvider)
-            if (takePictureIntent.resolveActivity(this.packageManager) != null) {
-                startActivityForResult(takePictureIntent, SELECT_ACTIVITY2)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    requestPermissions(permission, PERMISSION_CODE)
+                }else {
+                    openCamera()
+                }
+            }else{
+                openCamera()
             }
+
         }
         cargarGaleria.setOnClickListener {
             ImgController.selectPhotofromGallery(this,SELECT_ACTIVITY)
         }
     }
 
-    private fun getPhotoFile(fileName: String): File{
-        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(fileName, ".jpg", storageDir)
+    private fun openCamera(){
+        val values= ContentValues()
+        values.put(MediaStore.Images.Media.TITLE,"New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION,"From the camera")
+        imageUri2=contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
+        val cameraIntent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri2)
+        startActivityForResult(cameraIntent,IMAGE_CAPTURE_CODE)
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode){
+            PERMISSION_CODE->{
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED ) {
+                    openCamera()
+                }else{
+                    Toast.makeText(this,"Permiso denegado",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
-
+        if (requestCode == IMAGE_CAPTURE_CODE && resultCode==Activity.RESULT_OK){
+            imageSelect_iv.setImageURI(imageUri2)
+        }
         if (requestCode == SELECT_ACTIVITY && resultCode == Activity.RESULT_OK) {
             imageUri=data!!.data
             imageSelect_iv.setImageURI(imageUri)
         }
-        if (requestCode== SELECT_ACTIVITY2 && resultCode == Activity.RESULT_OK){
-            val takenImg = BitmapFactory.decodeFile(photoFile.absolutePath)
-            imageSelect_iv.setImageBitmap(takenImg)
-        }else{
-            super.onActivityResult(requestCode, resultCode, data)
-        }
+
     }
 /*
         val bundle= intent.extras
